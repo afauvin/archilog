991364602b13147da06216e9812d39cfde604c5d
fbalfouong <florentin.balfouong@gipatgeri.fr>
2023-03-05 09:40:37 +0100

    GUIHelper class relocalized

--- src/main/java/xshape/AwtApp.java
+++ src/main/java/xshape/AwtApp.java
@@ -2,28 +2,11 @@ package xshape;
 
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
 
 import ui.factories.ShapeFactory;
 import ui.factories.awt.ShapeFactoryAwt;
 
-class GUIHelper {
-    public static void showOnFrame(JComponent component, String frameName) {
-        JFrame frame = new JFrame(frameName);
-        WindowAdapter wa = new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        };
-        frame.addWindowListener(wa);
-        frame.getContentPane().add(component);
-        frame.pack();
-        frame.setVisible(true);
-    }
-}
+
 
 public class AwtApp extends XShape {
     class JCanvas extends JPanel {
