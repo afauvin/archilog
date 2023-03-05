08ad62f2d6d0c1b05fcdd0b9c452fac481b30d79
fbalfouong <florentin.balfouong@gipatgeri.fr>
2023-03-05 09:49:07 +0100

    Basic Test architecture added

---
+++ src/test/java/ui/UiTest.java
@@ -0,0 +1,24 @@
+package ui;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Objects;
+
+import org.junit.Test;
+import org.junit.Test.None;
+
+/**
+ * Unit test for UI.
+ */
+public class UiTest 
+{
+    /**
+     * 
+     */
+    @Test
+    public void uiUselessTest()
+    {
+        assertTrue( true );
+    }
+}
